25ecb550cf001957ee2b006954a9b55994fd4396
Marcos <Marcos@Marcos>
2015-02-12 10:01:51 +0100

    final

---
+++ src/Paquete_cod/EntradaDatos.java
@@ -0,0 +1,35 @@
+
+package Paquete_cod;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class EntradaDatos {
+
+    
+  public int introducir (int opcion){
+    int num;
+      switch(opcion){
+ 
+       case 0:
+          System.out.println("introduce el dato");          
+          Scanner scan=new Scanner(System.in);
+          num= scan.nextInt();
+          break;
+       case 1:
+      
+      
+         String respuesta =JOptionPane.showInputDialog("introducir los datos");
+          num=Integer.parseInt(respuesta);
+          break;
+  
+       default:
+        
+        JOptionPane.showInputDialog("error, elige o 0 o 1",JOptionPane.WARNING_MESSAGE);
+   
+   }
+      return opcion;
+           
+}
+}
